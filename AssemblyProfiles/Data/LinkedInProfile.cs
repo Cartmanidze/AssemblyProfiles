2ab6bfeffb276185ea5290dcb9297832f3126b8e
Глеб Безруков <Gleb.Bezrukov@galaktikapro.onmicrosoft.com>
2020-04-12 16:08:20 +0300

    Реализация профиля LinkedIn

---
+++ AssemblyProfiles/Data/LinkedInProfile.cs
@@ -0,0 +1,33 @@
+﻿using AssemblyProfiles.Core.Storage;
+using System.Collections.Generic;
+
+namespace AssemblyProfiles.Data
+{
+    public class LinkedInProfile : IProfile
+    {
+        public LinkedInProfile()
+        {
+        }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Контакты
+        /// </summary>
+        public string Contacts { get; set; }
+        /// <summary>
+        /// Образование
+        /// </summary>
+        public List<string> Education { get; set; }
+        /// <summary>
+        /// Опыт
+        /// </summary>
+        public List<string> Experience { get; set; }
+
+    }
+
+}
+
+
