9d4a24d5f6d56e2a9c43c748b11bc4fd7b96c380
Глеб Безруков <Gleb.Bezrukov@galaktikapro.onmicrosoft.com>
2020-04-12 16:06:16 +0300

    Добавление класса регистрации фабрики

---
+++ AssemblyProfiles.Core/RegisterFactory/RegisterFactory.cs
@@ -0,0 +1,30 @@
+﻿using AssemblyProfiles.Core.Factory;
+using AssemblyProfiles.Core.Services.ApiService;
+using AssemblyProfiles.Core.Storage;
+using System;
+using System.Collections.Generic;
+
+namespace AssemblyProfiles.Core.RegisterFactory
+{
+    public static class RegisterFactory
+    {
+        public static Dictionary<IFactoryNetwork, IProfile> RegisteredFactory { get; set; } =  new Dictionary<IFactoryNetwork, IProfile>();
+
+        /// <summary>
+        /// Регистрация фабрики
+        /// </summary>
+        /// <typeparam name="TFactory">Тип фабрики</typeparam>
+        /// <typeparam name="TProfile">Тип профиля</typeparam>
+        /// <param name="apiService">Сервис Api</param>
+        public static void Register<TFactory, TProfile>(IApiService apiService) 
+            where TFactory : class, IFactoryNetwork
+            where TProfile : IProfile
+        {
+            TFactory factory = (TFactory)Activator
+                .CreateInstance(typeof(TFactory), new object[] { apiService });
+            TProfile profile = (TProfile)Activator
+                .CreateInstance(typeof(TProfile), new object[] {});
+            RegisteredFactory.Add(factory, profile);
+        }
+    }
+}
