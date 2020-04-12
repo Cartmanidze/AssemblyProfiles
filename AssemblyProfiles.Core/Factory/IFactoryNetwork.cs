74704455cdd757ecc6f6f020550ae022eadc44d6
Глеб Безруков <Gleb.Bezrukov@galaktikapro.onmicrosoft.com>
2020-04-12 16:04:38 +0300

    Добавление описания интерфейса фабрики

---
+++ AssemblyProfiles.Core/Factory/IFactoryNetwork.cs
@@ -0,0 +1,26 @@
+﻿using AssemblyProfiles.Core.Services;
+using AssemblyProfiles.Core.Services.HtmlParseService;
+
+namespace AssemblyProfiles.Core.Factory
+{
+    public interface IFactoryNetwork
+    {
+        /// <summary>
+        /// Создание сервиса для Selenium
+        /// </summary>
+        /// <returns></returns>
+        ISeleniumService CreateSeleniumService();
+
+        /// <summary>
+        /// Создание парсера Html
+        /// </summary>
+        /// <returns></returns>
+        IHtmlParseService CreateHtmlParseService();
+
+        /// <summary>
+        /// Создание сервиса записи
+        /// </summary>
+        /// <returns></returns>
+        IRecordingService CreateRecordingService();
+    }
+}
